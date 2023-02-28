30069da712306938c17ce4d504f2efce683ed138
Heezol06 <shahriarrahmanheezol@gmail.com>
2023-03-01 01:02:16 +0600

    nav,contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './Resources/Components/Common/Navbar';
+import Contacts from './Resources/Pages/DashboardPage/Contact';
 
 function App() {
   return (
     <div>
-
+<Navbar></Navbar>
+<Contacts></Contacts>
     </div>
   );
 }
